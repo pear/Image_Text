d1771e207d06d651615320862e628f9f294f4506
Michael Cramer <michael@bigmichi1.de>
2013-08-06 10:24:19 +0200

    document exception class

--- src/Image/Text/Exception.php
+++ src/Image/Text/Exception.php
@@ -1,2 +1,27 @@
 <?php
-class Image_Text_Exception extends Exception {}
+/**
+ * Exception for Image_Text
+ *
+ * PHP version 5
+ *
+ * @category Image
+ * @package  Image_Text
+ * @author   Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt PHP License
+ * @link     http://pear.php.net/package/Image_Text
+ */
+require_once 'PEAR/Exception.php';
+/**
+ * Exception for Image_Text
+ *
+ * @category Image
+ * @package  Image_Text
+ * @author   Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @author   Michael Cramer <michael@bigmichi1.de>
+ * @license  http://www.php.net/license/3_01.txt PHP License
+ * @link     http://pear.php.net/package/Image_Text
+ */
+class Image_Text_Exception extends PEAR_Exception
+{
+
+}
