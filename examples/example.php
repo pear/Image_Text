9fbce711dc6f73a233926fff0f9569798a8b562b
Michael Cramer <michael@bigmichi1.de>
2013-08-08 10:24:48 +0200

    some fixes for the existing example

--- examples/example.php
+++ examples/example.php
@@ -1,13 +1,10 @@
 <?php
-
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
 /**
  * Image_Text example.
  *
  * Example file for the usage of Image_Text.
  *
- * PHP versions 4 and 5
+ * PHP versions 5
  *
  * LICENSE: This source file is subject to version 3.0 of the PHP license
  * that is available through the world-wide-web at the following URI:
@@ -15,25 +12,16 @@
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
- * @category   Image
- * @package    Text
- * @author     Tobias Schlitt <toby@php.net>
- * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    CVS: $Id$
- * @link       http://pear.php.net/package/Net_FTP2
- * @since      File available since Release 0.0.1
+ * @category  Image
+ * @package   Image_Text
+ * @author    Tobias Schlitt <toby@php.net>
+ * @copyright 1997-2005 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt PHP License
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Image_Text
+ * @since     File available since Release 0.0.1
  */
-
-// ini_set('include_path', '.:/cvs/pear/Image_Text:/usr/share/pear');
-
 require_once 'Image/Text.php';
-
-function getmicrotime()
-{
-    list($usec,$sec) = explode(' ', microtime());
-    return ((float)$usec+(float)$sec);
-}
 
 $colors = array(
     0 => '#0d54e2',
@@ -65,43 +53,39 @@ $options = array(
             'font_path'     => './',
             'antialias'     => true,
             'font_file'     => 'Vera.ttf',
-            'halign'        => IMAGE_TEXT_ALIGN_LEFT
+            'halign'        => Image_Text::IMAGE_TEXT_ALIGN_LEFT
         );
 
 $text = $texts[array_rand($texts)];
 
 foreach ($_GET as $key => $val) {
     switch ($key) {
-        case 'font_size':
-            $options['font_size'] = (int)$val;
-            break;
-        case 'line_spacing':
-            $options['line_spacing'] = $val;
-            break;
-        case 'halign':
-            $options['halign'] = $val;
-            break;
-        case 'valign':
-            $options['valign'] = $val;
-            break;
-        case 'angle':
-            $options['angle'] = $val;
-            break;
-        case 'text':
-            $text = $texts[$val];
-            break;
+    case 'font_size':
+        $options['font_size'] = (int)$val;
+        break;
+    case 'line_spacing':
+        $options['line_spacing'] = $val;
+        break;
+    case 'halign':
+        $options['halign'] = $val;
+        break;
+    case 'valign':
+        $options['valign'] = $val;
+        break;
+    case 'angle':
+        $options['angle'] = $val;
+        break;
+    case 'text':
+        $text = $texts[$val];
+        break;
     }
 }
 
-$start = getmicrotime();
+$start = microtime(true);
 
 $itext = new Image_Text($text, $options);
 
-$ret = $itext->init();
-if (PEAR::isError($ret)) {
-    echo $ret->getMessage() . "\n";
-    die();
-}
+$itext->init();
 
 if (empty($options['font_size'])) {
     $itext->autoMeasurize();
@@ -111,15 +95,14 @@ if (empty($options['font_size'])) {
 
 $itext->render();
 
-$end = getmicrotime();
+$end = microtime(true);
 
 $time = $end - $start;
 
 if (isset($_GET['debug'])) {
     echo "<h1>-- DEBUGGING MODE --</h1>";
 } else {
-
-    $img =& $itext->getImg();
+    $img = $itext->getImg();
 
     $red = imagecolorallocate($img, 255, 0, 0);
 
@@ -130,7 +113,4 @@ if (isset($_GET['debug'])) {
     }
 
     $itext->display();
-
 }
-
-?>
